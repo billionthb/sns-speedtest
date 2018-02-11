e3d3c31d0e1ad5082d191749b95f3dd3a18a8154
William Steele <billionthb@yahoo.com>
2018-02-11 18:53:55 -0500

    XML Config and python path correction

--- spdtst_install.bat
+++ spdtst_install.bat
@@ -9,7 +9,7 @@ echo installing Python 3 and script prerequisites
 pyinstall.exe PrependPath=1 /quiet
 rem installs python 3.6.4, adds to windows path variable, without popping up UI
 
-cd C:\Users\jsnider\AppData\Local\Programs\Python\Python36
+cd %HOMEPATH%\AppData\Local\Programs\Python\Python36
 python -m pip install pymysql
 python -m pip install speedtest-cli
 
@@ -30,8 +30,10 @@ rem to hard code any of the above, simply remove the /p flag and change the prom
 	echo python %HOMEPATH%\Downloads\spdtstdb.py "%location%" %ip% %id% %pw% %db% %tb%
 )
 
-set /p rep="How long is the test interval, in minutes: "
+rem set /p rep="How long is the test interval, in minutes: "
 echo Now creating task to run speedtest automatically.
 echo This will run under current user, and will ask for account password
 echo If current user does not have ADMIN privileges, this may not work
-schtasks /create /RU SYSTEM /SC ONSTART /TN Speedtest /TR C:/AUTORUN/speedtest.bat /RI %rep% /RL HIGHEST /F
+powershell -Command "(New-Object Net.WebClient).DownloadFile('https://drive.google.com/uc?export=download&id=1OmnGG13uVe5Vz3XsvnxxG5j9K5oPTw5q', 'speedtest.xml')"
+rem schtasks /create /RU SYSTEM /SC ONSTART /TN Speedtest /TR C:/AUTORUN/speedtest.bat /RI %rep% /RL HIGHEST /F
+schtasks /create /TN speedtest /RU %USERNAME% /XML speedtest.xml
